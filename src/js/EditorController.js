072562d35285713bc2f29f9d9b31927e4cdd88e4
hepp <hepp@audiopixel.com>
2015-01-10 12:55:32 -0700

    Editor Controller w test View

--- src/js/EditorController.js
+++ src/js/EditorController.js
@@ -9,16 +9,29 @@
 var EditorController = function ( ap ) {
 
 	this.ap = ap;
+	this.editor = this.ap.editor;
+
+	this.meshCount = 0;
 
 };
 
 EditorController.prototype = {
 
-	init: function () {
-		
-	},
+	addTestPlane: function () {
 
-	update: function () {
+		var width = 200;
+		var height = 200;
+
+		var widthSegments = 1;
+		var heightSegments = 1;
+
+		var geometry = new THREE.PlaneGeometry( width, height, widthSegments, heightSegments );
+		var material = new THREE.MeshPhongMaterial();
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.name = 'ApPlane ' + ( ++ this.meshCount );
+
+		this.editor.addObject( mesh );
+		this.editor.select( mesh );
 
 	}
 
