85dca31860a83bf6e13764dab0e1cac9ea6b46e7
hepp <hepp@audiopixel.com>
2015-01-14 22:01:01 -0700

    PodManager pseudo

--- src/js/managers/PodManager.js
+++ src/js/managers/PodManager.js
@@ -1,8 +1,9 @@
 /*
  * ************* POD MANAGER *************** 
- * Handles the state of all Pods running in the universe
- * Pods are organized in Channels, and may contain a clips
+ * Handles the state of all Pods running in the Universe
+ * Pods are organized in Channels, and may contain Clips
  *
+ * (should this be refactored to ChannelManager ? , might make more sense in JS obj land)
  */
 
 var PodManager = function () {
@@ -14,17 +15,42 @@ var PodManager = function () {
 	--hold state:
 	---------------------------------------
 
-	any number of pod objects
-		id
-		address info: channel, pod
-		position group id (that this pod references position data from)
-		any number of clip objects
-			address info: clip
-			clip id
-			param values
-			mod values
+	pod objects stored on channel objects
 
-	any number of position group objects (each pod must point to one of these)
+		channels = [];
+		channels[0] = new Channel(); // 0 is the address. associative/holey arrays ok, its only looped once when we regenerate shader 
+		channels[0].type = "content"; // or 'fx' and 'scenefx' eventually
+		channels[0].pods = [];
+		channels[0].pods[0] = new Pod(); // '0.0' is the 'channel.pod' address
+		channels[0].pods[0].mix = 1;
+		channels[0].pods[0].clips = [];
+		channels[0].pods[0].clips[0] = new Clip(); // '0.0.0' is the 'channel.pod.clip' address
+		channels[0].pods[0].clips[0].id = 12; // colorwash clip for example
+
+
+
+	any number of channel objects
+		type (content, fx, or scene)
+		mix value
+		mod values (just for mix)
+
+		any number of pod objects
+			mix value
+			blend value
+			mod values (just for mix)
+			position group id (that this pod references position data from)
+			hardware group id (up to 3 of them)
+				exclude or solo mode (for all hardware groups)
+
+			any number of clip objects
+				mix value
+				blend value
+				param values
+				mod values (for mix and params)
+				clip id (so we know which shader to grab from library)
+
+
+	any number of position group objects (each pod must point to one of these) (maybe refactor into seperate manager?)
 		id
 		position data: xyz, whd
 
@@ -32,10 +58,17 @@ var PodManager = function () {
 	--responsibilites:
 	---------------------------------------
 
-	main responsibility is to provide shader snippets with pod and clip data
-		this is re-generated to shader anytime a pod/channel gets added or deleted, or position or address change
+	main responsibility is to build source shader from 'snippets that have pod and clip data baked in'
+		the shader gets re-generated anytime
+			a pod gets added or deleted
+			a pod changes it's hardware group(s), or how it uses the hardware group(s) (exclude or solo) 
+			a pod changes which position group it references
+			a position group's coordinates change (if actively referenced by a pod)
 
-	when in editor mode, show all the pod position groups, record any changes
+	define shader uniforms (to be used as clip params and values)
+
+
+	when in editor mode, show all the pod position groups, record any coordinate changes
 
 
 	*/
