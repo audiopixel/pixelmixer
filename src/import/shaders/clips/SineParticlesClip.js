943f99e1ddcd99a57235bf5bcbed002b26d5d827
hepp <hepp@audiopixel.com>
2015-03-10 20:43:28 -0600

    Shaders reference time individually

--- src/import/shaders/clips/SineParticlesClip.js
+++ src/import/shaders/clips/SineParticlesClip.js
@@ -12,7 +12,7 @@ ap.clips.SineParticlesClip = {
 
 	fragmentFunctions: [
 
-		[ "float  metaball ( vec2 pos, float offset) {",
+		[ "float  metaball ( vec2 pos, float offset, float time) {",
 				"float t = time + offset;",
 				"vec2 metaballPos = vec2(sin(t * .8), cos(t));",
 				"return 1. / length(pos - metaballPos);",
@@ -28,7 +28,7 @@ ap.clips.SineParticlesClip = {
 
 		"float cf = 0.;",
 		"for(int i = 0; i < 20; i++) {",
-			"cf += metaball(p, float(i) / 5.) / 20.;",
+			"cf += metaball(p, float(i) / 5., time) / 20.;",
 		"}",
 
 		"gl_FragColor = vec4( vec3( cf, cf * 0.5, sin( cf + time / 3.0 ) * 0.75 ), 1.0 );"
