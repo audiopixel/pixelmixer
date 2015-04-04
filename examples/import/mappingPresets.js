e99b427400606a1a433d5d2f6861fe687015df89
hepp <connect@audiopixel.com>
2015-04-04 14:54:03 -0600

    Example mapping presets

---
+++ examples/import/mappingPresets.js
@@ -0,0 +1,71 @@
+PX.datguiJson = {
+  "preset": "test7",
+  "remembered": {
+    "Spiral 1": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 1",
+        "Shader": "SinSpiral",
+        "Scale": 1
+      }
+    },
+    "Spiral 2": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 2",
+        "Shader": "SinSpiral",
+        "Scale": 0.28533230298688494
+      }
+    },
+    "Spiral 3": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 3",
+        "Shader": "SinSpiral",
+        "Scale": 0.1
+      }
+    },
+    "SinCosTan 1": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 3",
+        "Shader": "SinCosTan",
+        "Scale": 0.402384283820707
+      }
+    },
+    "SinCosTan 2": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 5",
+        "Shader": "SinCosTan",
+        "Scale": 0.6072252502798957
+      }
+    },
+    "SineBand": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 4",
+        "Shader": "ColorSineBar",
+        "Scale": 0.402384283820707
+      }
+    },
+    "SineParticles": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 2",
+        "Shader": "SineParticles",
+        "Scale": 0.20729764909767023
+      }
+    },
+    "SimpleMirror": {
+      "0": {
+        "Channel1Mix": 1,
+        "Mapping": "Layout 6",
+        "Shader": "SimpleSwirl",
+        "Scale": 1
+      }
+    }
+  },
+  "closed": false,
+  "folders": {}
+}
