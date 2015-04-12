3b59c55a5838669e3d529b15ed3abb54c7457ec5
hepp <info@audiopixel.com>
2015-04-12 17:29:53 -0600

    Cleanup. 1 q q

--- examples/chrome_app/libs/pixelmixer.js
+++ examples/chrome_app/libs/pixelmixer.js
@@ -1,26 +1,3 @@
-/*
-The MIT License
-
-Copyright &copy; 2010-2015 Hepp Maccoy, AudioPixel, & PixelMixer.js Contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 
 var PX = { version: '0.1.0' };	// Global PixelMixer object
 
@@ -109,8 +86,10 @@ PX.MAP_ALT2 = 2;
 PX.shaderCount = -1;
 PX.init = function(scene, renderer, params){
 
-	PX.broadcast = params.broadcast || PX.broadcast;
+	params = params || {};
+
 	PX.readPixels = params.readPixels || PX.readPixels;
+	PX.broadcast = params.broadcast || PX.broadcast;
 	PX.pointSize = params.pointSize || PX.pointSize;
 	PX.pointSprite = params.pointSprite || PX.pointSprite;
 	PX.useTransforms = params.useTransforms || PX.useTransforms;
